2922dc21aea8b17c56f0136239878cf41b0e4edc
anthonypileggi <anthony@ifixit.com>
2019-01-28 17:37:33 -0500

    use US/Pacific timezone

--- shipstationr/R/ss_parse_datetime.R
+++ shipstationr/R/ss_parse_datetime.R
@@ -2,6 +2,6 @@
 #' Parse a date-time vector
 #' @param x vector of date-times
 ss_parse_datetime <- function(x) {
-  x <- as.POSIXct(strptime(x, "%Y-%m-%dT%H:%M:%OS"), tz = "US/Mountain")
+  x <- as.POSIXct(strptime(x, "%Y-%m-%dT%H:%M:%OS"), tz = "US/Pacific")
   lubridate::with_tz(x, tzone = Sys.timezone())
 }
