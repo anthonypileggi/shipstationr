542f859f87148589f20b1e4237166a8a7ad22dd1
anthonypileggi <anthony@ifixit.com>
2019-02-04 16:45:59 -0500

    include 'packageCode' dimension

--- shipstationr/R/ss_get_single_item_shipping_weights.R
+++ shipstationr/R/ss_get_single_item_shipping_weights.R
@@ -1,14 +1,15 @@
 #' Estimate product weights based on shipping weights for recent single-item orders
 #' @param start_date first day of data (Date/scalar)
 #' @param end_date last day of data (Date/scalar)
+#' @importFrom magrittr "%>%"
 #' @export
 ss_get_single_item_shipping_weights <- function(start_date = Sys.Date() - 7, end_date = Sys.Date() - 1) {
   
-  # TODO: add 'package_type' and 'dimensions'
+  # TODO: add 'dimensions'
   
   # get total weights (lbs) for all recent shipments
-  shipments <- shipstationr::ss_get_shipments(start_date = start_date, end_date = end_date) %>%
-    dplyr::select(orderNumber, weight, shipmentCost) %>%
+  shipments <- ss_get_shipments(start_date = start_date, end_date = end_date) %>%
+    dplyr::select(orderNumber, weight, shipmentCost, packageCode) %>%
     tidyr::unnest() %>%
     dplyr::mutate(
       value = dplyr::case_when(
@@ -16,7 +17,7 @@ ss_get_single_item_shipping_weights <- function(start_date = Sys.Date() - 7, end
         TRUE ~ value
       )
     ) %>%
-    dplyr::select(orderNumber, ship_weight = value, ship_cost = shipmentCost)
+    dplyr::select(orderNumber, ship_weight = value, ship_cost = shipmentCost, packageCode)
 
   # identify shipment contents and keep only single-item orders
   ss_get_orders(start_date = start_date - 1, end_date = end_date + 1) %>%
@@ -35,6 +36,6 @@ ss_get_single_item_shipping_weights <- function(start_date = Sys.Date() - 7, end
       )
     ) %>%
     dplyr::filter(quantity == 1) %>%                               # only count qty=1 orders (TODO: factor in qty>1 orders)
-    dplyr::select(orderNumber, sku, skuvault_weight = value) %>%
+    dplyr::select(orderNumber, sku, ca_weight = value) %>%
     dplyr::left_join(shipments, by = "orderNumber")
 }
