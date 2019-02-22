afad75d68eebd2770db6e0ec2ab9a584160a72b8
Anthony Pileggi <apileggi20@gmail.com>
2019-02-22 14:18:54 -0500

    add plotting option

--- shipstationr/R/ss_get_shipping_speed.R
+++ shipstationr/R/ss_get_shipping_speed.R
@@ -1,9 +1,12 @@
 #' Get shipping speed (overall; user-specific)
 #' @param start_date first day of data (Date/scalar)
 #' @param end_date last day of data (Date/scalar)
+#' @param show_plot show time-series plot (logical/scalar)
 #' @importFrom magrittr "%>%"
 #' @export
-ss_get_shipping_speed <- function(start_date = Sys.Date(), end_date = Sys.Date()) {
+ss_get_shipping_speed <- function(start_date = Sys.Date(), 
+                                  end_date = Sys.Date(),
+                                  show_plot = TRUE) {
   
   users <- ss_get_users()
   shipments <- ss_get_shipments(start_date = start_date, end_date = end_date) %>%
@@ -35,6 +38,22 @@ ss_get_shipping_speed <- function(start_date = Sys.Date(), end_date = Sys.Date()
       avg = mean(diff, na.rm = TRUE),
       recent = mean(tail(diff, 10), na.rm = TRUE)
     )
+  
+  # plot time-series
+  if (show_plot) {
+    require(ggplot2)
+    g1 <- shipments %>%
+      dplyr::left_join(users, by = "userId") %>%
+      dplyr::group_by(lubridate::date(createDate), name) %>%
+      dplyr::mutate(
+        count = dplyr::row_number()
+      ) %>%
+      ggplot(aes(x = createDate, y = count, group = name, color = name)) +
+      geom_point() +
+      labs(x = NULL, y = "Total Shipped", color = NULL) +
+      theme_bw()
+    print(g1)
+  }
   
   dplyr::bind_rows(out2, out1)
 }
