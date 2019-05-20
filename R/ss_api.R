e5a0db1d3e4e18bd3f5013c1f6ab9dd2a10349d7
anthonypileggi <anthony@ifixit.com>
2019-05-20 15:37:00 -0400

    allow for GET or POST

--- R/ss_api.R
+++ R/ss_api.R
@@ -1,16 +1,20 @@
 #' Make a ShipStation API call
 #' @param path path to api endpoint
+#' @param fun API function (GET or POST)
 #' @export
-ss_api <- function(path, ...) {
-  
+ss_api <- function(path, fun = httr::GET, ...) {
+
   # setup creds
   if (nchar(Sys.getenv("SHIPSTATION_API_KEY")) == 0 | nchar(Sys.getenv("SHIPSTATION_API_SECRET")) == 0)
     stop("You must set environment variables: SHIPSTATION_API_KEY, SHIPSTATION_API_SECRET", call. = FALSE)
-  
+
   # call api
   url <- httr::modify_url("https://ssapi.shipstation.com", path = path)
   auth <- httr::authenticate(Sys.getenv("SHIPSTATION_API_KEY"), Sys.getenv("SHIPSTATION_API_SECRET"))
-  response <- httr::GET(url, auth, query = list(...))
+  if (identical(fun, httr::POST))
+    response <- fun(url, auth, httr::content_type_json(), body = list(...), encode = "json")
+  if (identical(fun, httr::GET))
+    response <- fun(url, auth, query = list(...))
 
   # check status code
   if (httr::status_code(response) != 200) {
@@ -22,6 +26,6 @@ ss_api <- function(path, ...) {
       call. = FALSE
     )
   }
-  
+
   httr::content(response)
 }
