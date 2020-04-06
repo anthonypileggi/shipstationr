20c6f283a9897a63c1f4ed69d900e884af38efe8
anthonypileggi <anthony@ifixit.com>
2020-04-05 20:06:22 -0400

    robustify rate calculator

--- R/ss_get_rates.R
+++ R/ss_get_rates.R
@@ -2,10 +2,21 @@
 #' Get shipping rates for ALL carriers for a given order
 #' @param order_number order number (numeric/scalar)
 #' @param carriers carriers (character/vector)
+#' @param speed shipping speed (character/scalar)
 #' @export
 ss_get_rates <- function(order_number = "3023035",
-                         carriers = c("stamps_com", "ups")) {
+                         carriers = c("stamps_com", "ups", "fedex", "amazon_shipping", "amazon_shipping_us"),
+                         speed = c("standard", "2day", "1day")) {
+
+  # set shipping speed restrictions (serviceCode)
+  speed <- match.arg(speed)
+
+  # TODO: set package restrictions (if has weight/dimensions)
+  # TODO: set carrier restrictions (based on marketplace)
+  # TODO: set ground/air restrictions (liquids/batteries)
+
   p <- ss_get_orders(order_number = order_number)
+
   out <-
     purrr::map_df(
       carriers,
@@ -14,17 +25,23 @@ ss_get_rates <- function(order_number = "3023035",
         fromPostalCode = "18901",
         toPostalCode = stringr::str_split(p$shipTo[[1]]$postalCode, "-")[[1]][1],
         toCountry = p$shipTo[[1]]$country,
-        weight = list(value = p$weight[[1]]$value, units = p$weight[[1]]$units),
-        packageCode = "package",
-        # TODO: only add dimensions when available!!!
-        dimensions = list(
-          length = p$dimensions[[1]]$length,
-          width = p$dimensions[[1]]$width,
-          height = p$dimensions[[1]]$height,
-          units = "inches"
-          )
+        #weight = list(value = p$weight[[1]]$value, units = p$weight[[1]]$units),
+        weight = as.list(p$weight[[1]]),
+        serviceCode = dplyr::case_when(
+          .x == "amazon_shipping_us" ~ "amazon_shipping_standard",
+          TRUE ~ NA_character_
+        ),
+        packageCode = dplyr::case_when(
+          .x == "amazon_shipping_us" ~ "package",
+          TRUE ~ NA_character_
+        ),
+        dimensions = as.list(p$dimensions[[1]]),
+        confirmation = dplyr::case_when(
+          .x == "amazon_shipping_us" ~ "none",
+          TRUE ~ NA_character_
+        )
       )
-    ) %>%
+    )
   dplyr::arrange(out, shipmentCost)
 }
 
