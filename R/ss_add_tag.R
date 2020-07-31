f6b3cbd71fef69004c24943cea49b7bf8d758074
anthonypileggi <apileggi20@gmail.com>
2020-07-31 15:15:01 -0400

    stop execution if no order_number is provided as input

--- R/ss_add_tag.R
+++ R/ss_add_tag.R
@@ -2,18 +2,20 @@
 #' @param order_number order number (character/scalar)
 #' @param tag_id tag id (numeric/scalar)
 #' @export
-ss_add_tag <- function(order_number, tag_id = NULL) {
-  
+ss_add_tag <- function(order_number = NULL, tag_id = NULL) {
+
   if (is.null(tag_id))
     stop("You must specify the tag to use!")
-  
+  if (is.null(order_number))
+    stop("You must specify the `order_number` to tag!")
+
   # get orderId for the specified order
   order <- shipstationr::ss_get_orders(order_number = order_number)
-  
+
   if (nrow(order) == 1) {
-    out <- 
+    out <-
       shipstationr::ss_api(
-        path = "orders/addtag", 
+        path = "orders/addtag",
         fun = httr::POST,
         orderId = order$orderId,
         tagId = 95540
@@ -21,7 +23,7 @@ ss_add_tag <- function(order_number, tag_id = NULL) {
   } else {
     out <- NULL
   }
-  
+
   return(invisible(out))
 }
 
